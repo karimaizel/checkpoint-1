37ea407c900b94222b9039913344645924c094f3
karimaizel <karim.aizel4321@gmail.com>
2024-05-24 12:57:27 +0100

    my new html project

---
+++ html/js/test.js
@@ -0,0 +1,17 @@
+var gimli ={
+name: "gimli"
+race:"dwarf"
+weapon:
+color_changed:false
+color:"red"
+change color: function(){
+    if (this.color_changed){
+        this.color ="green"
+    }
+}
+
+
+
+
+
+}
